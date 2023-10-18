1d6a48be0b74feeb214dc34d6be9e04574e5014b
JoulePhi <dzulfikar.sadid.khoir@gmail.com>
2023-10-18 07:43:05 +0700

    fix variable

--- resources/views/soap-response.blade.php
+++ resources/views/soap-response.blade.php
@@ -12,7 +12,7 @@
 
         <div class="dropdown mb-5">
             <button class="btn btn-primary dropdown-toggle" type="button" id="dropdownMenuButton" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              {{  $selecteddate ? $selecteddate : $dates->last()->date }}
+              {{  isset($selecteddate) ? $selecteddate : $dates->last()->date }}
             </button>
             <div class="dropdown-menu" aria-labelledby="dropdownMenuButton">
                 @foreach ($dates as $date)
